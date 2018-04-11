160b364d230a82ecd1555d3b069b1c751f1057dc
workstation <ws@C46.local.bmstu.ru>
2018-04-11 11:20:15 +0400

    next

--- Lab2_V19/Lab2_V19.c
+++ Lab2_V19/Lab2_V19.c
@@ -1,4 +1,4 @@
-﻿#include <stdlib.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,12 +9,11 @@
  
 
 
-#define _POSIX_BARRIERS 1
 
-#define F(x,t)  25.0
+#define F(x,t)  1e2
 #define DELTA_T 1.0
 #define DELTA_X 1.0
-#define A       1.0
+#define A       0.1
  
 
  
@@ -78,7 +77,7 @@ void* mysolver (void *arg_p)
  
     for (i = thr->first; i <= thr->last; i++)
     {
-      if ( i == (node-2) / 2 && CurrentTime < nc/20 )
+      if ( i == (node-2) / 2 && CurrentTime < 10 )
         f = F(i,CurrentTime);
       else
         f = 0;
@@ -95,12 +94,11 @@ void* mysolver (void *arg_p)
 void calculateFisrtTime(int nc)
 {
   int i;
+   for(i = 1; i < node-1; i++)
+     Z[0][i] = Z[1][i] = 0;
  
-  for(i = 1; i < node-1; i++)
-    Z[0][i] = Z[1][i] = 0;
- 
-  for(i = 0; i < 2; i++)
-    writeIntoFile(i);
+   for(i = 0; i < 2; i++)
+     writeIntoFile(i);
 }
  
 int main (int argc, char* argv[]) 
@@ -115,7 +113,7 @@ int main (int argc, char* argv[])
   nc = atoi(argv[2]);       // временной интервал  
   node = atoi(argv[3]);     // количество узлов
  
-  if ( (node-2) % nt != 0 )
+  /*if ( (node-2) % nt != 0 )
   {
     printf("enter kratnoe: %d\n",(node-2));
     scanf("%d",&nt);
@@ -123,12 +121,13 @@ int main (int argc, char* argv[])
  
   if ( nt >= node-2 ) 
     nt = node-2;
- 
+  set yrange[0:70]
+ */
   out = fopen("out.txt","w");
   fp = fopen("vgraph0.dat","w");              //открытие файла для графического представления в gnuplot
-  fprintf(fp,"set xrange[0:%d]\nset yrange[0:70]\n", node-1);
+  fprintf(fp,"set xrange[0:%d]\n\n", node-1);
   fprintf(fp, "do for [i=0:%d]{\n",nc/precision-1 );
-  fprintf(fp,"plot 'out.txt' index i using 1:2 smooth bezier title 'powered by Seva'\npause 0.5}\npause -1\n");
+  fprintf(fp,"plot 'out.txt' index i using 1:2 smooth bezier title '123'\npause 0.02}\npause -1\n");
  
   int i,k;
   Z = (int **) malloc (2 * sizeof(int*)); 
